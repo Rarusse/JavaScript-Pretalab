64f36819f6a6f4e2953877f1486213f8cbb91692
Rarusse <168042857+Rarusse@users.noreply.github.com>
2024-05-22 15:13:43 -0300

    Adding JS file

---
+++ estrutura-condicional.js
@@ -0,0 +1,20 @@
+const numero = 09;
+
+console.log(numero > 10);
+
+console.log(numero < 10);
+
+console.log(numero === 10);
+
+if (numero > 10) 
+{
+    console.log("Esse número é maior que 10");
+} 
+else if (numero < 10)
+{
+    console.log("Esse número é menor que 10");
+} 
+else 
+{
+    console.log("Esse número é 10");
+}
